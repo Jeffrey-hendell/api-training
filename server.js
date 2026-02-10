5df5e2e2875a2c1ced6dabcbd91ecfa6c0b67f18
JHY.Solutions <72576017+Jeffrey-hendell@users.noreply.github.com>
2026-02-09 20:21:49 -0400

    Comment out express.json middleware
    Comment out the JSON body parser middleware.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(helmet());
 app.use(cors());
-app.use(express.json({ limit: '1mb' }));
+// app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 
 app.use('/api/users', userRoutes);
